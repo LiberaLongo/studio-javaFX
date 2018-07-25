86bda1955195edd7527ce41c42f64d317086644d
liberalongo@gmail.com <liberalongo@gmail.com>
2018-07-25 21:13:04 +0200

    Topolino

--- Topolino.java
+++ Topolino.java
@@ -13,18 +13,18 @@ public class Topolino extends Application {
 		//variabili ampiezza schermo
 		Double widthScene = 1000.0, heightScene = 700.0;
 		//variabili coordinate del soggetto
-		Double x = widthScene*1/2, y = heightScene*2/3;
+		Double x = widthScene*1/2, y = heightScene*1/2;
 		//proporzioni fisiche del soggetto
 		//testa
 		Double raggioTesta = 200.0;
 		//orecchie
 		Double distanzaOrecchie = raggioTesta*3/4;
-		Double altezzaOrecchie = raggioTesta*2/3;
+		Double altezzaOrecchie = raggioTesta*1;
 		Double raggioOrecchie = raggioTesta*1/2;
 		//orbite
 		Double distanzaOrbite = raggioTesta*1/4;
 		Double radiusXOrbite = raggioTesta*1/3;
-		Double radiusYOrbite = raggioTesta*1/8;
+		Double radiusYOrbite = raggioTesta*1/2;
 		//occhi
 		Double distanzaOcchi = distanzaOrbite;
 		Double radiusXOcchi = radiusXOrbite*1/2;
@@ -48,9 +48,32 @@ public class Topolino extends Application {
 		Circle orecchioDestro = new Circle(x+altezzaOrecchie, y-distanzaOrecchie, raggioOrecchie);
 		Shape orecchie = Shape.union(orecchioSinistro, orecchioDestro);
 		Shape testa = Shape.union(cranio, orecchie);
+		testa.setFill(Color.BLACK);
+		//orbite
+		Ellipse orbitaSinistra = new Ellipse(x-distanzaOrbite, y, radiusXOrbite, radiusYOrbite);
+		Ellipse orbitaDestra = new Ellipse(x+distanzaOrbite, y, radiusXOrbite, radiusYOrbite);
+		Shape orbite = Shape.union(orbitaSinistra, orbitaDestra);
+		orbite.setFill(Color.PINK);
+		//occhi
+		Ellipse occhioSinistro = new Ellipse(x-distanzaOcchi, y, radiusXOcchi, radiusYOcchi);
+		Ellipse occhioDestro = new Ellipse(x+distanzaOcchi, y, radiusXOcchi, radiusYOcchi);
+		Shape occhi = Shape.union(occhioSinistro, occhioDestro);
+		occhi.setFill(Color.WHITE);
+		//pupille
+		Ellipse pupillaSinistra = new Ellipse(x-distanzaPupille, y, radiusXPupille, radiusYPupille);
+		Ellipse pupillaDestra = new Ellipse(x+distanzaPupille, y, radiusXPupille, radiusYPupille);
+		Shape pupille = Shape.union(pupillaSinistra, pupillaDestra);
+		pupille.setFill(Color.BLACK);
+		//bocca
+		Ellipse bocca = new Ellipse(x, y+altezzaBocca, radiusXBocca, radiusYBocca);
+		bocca.setFill(Color.PINK);
+		//naso
+		Ellipse naso = new Ellipse(x, y+altezzaNaso, radiusXNaso, radiusYNaso);
+		naso.setFill(Color.BLACK);
 
 		//stampa tutto
-		Group topolino = new Group(testa);
+		Group topolino = new Group();
+		topolino.getChildren().addAll(testa, orbite, occhi, pupille, bocca, naso);
 		Scene scene = new Scene(topolino ,widthScene, heightScene);
 		primaryStage.setTitle("Topolino");
 		primaryStage.setScene(scene);
