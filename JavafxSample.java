b973284f8b569fb074780acec7dcb3cffd00e6db
liberalongo@gmail.com <liberalongo@gmail.com>
2018-07-25 09:21:29 +0200

    application example 1 https://www.tutorialspoint.com/javafx/javafx_application.htm

--- JavafxSample.java
+++ JavafxSample.java
@@ -5,27 +5,28 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class JavafxSample extends Application {
-   @Override
-   public void start(Stage primaryStage) throws Exception {
-      //creating a Group object
-      Group group = new Group();
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		//creating a Group object
+		Group group = new Group();
 
-      //Creating a Scene by passing the group object, height and width
-      Scene scene = new Scene(group ,600, 300);
+		//Creating a Scene by passing the group object, height and width
+		Scene scene = new Scene(group ,600, 300);
 
-      //setting color to the scene
-      scene.setFill(Color.BROWN);
+		//setting color to the scene
+		scene.setFill(Color.BROWN);
 
-      //Setting the title to Stage.
-      primaryStage.setTitle("Sample Application");
+		//Setting the title to Stage.
+		primaryStage.setTitle("Sample Application");
 
-      //Adding the scene to Stage
-      primaryStage.setScene(scene);
+		//Adding the scene to Stage
+		primaryStage.setScene(scene);
 
-      //Displaying the contents of the stage
-      primaryStage.show();
-   }
-   public static void main(String args[]){
-      launch(args);
-   }
+		//Displaying the contents of the stage
+		primaryStage.show();
+	}
+
+	public static void main(String args[]){
+		launch(args);
+	}
 }
