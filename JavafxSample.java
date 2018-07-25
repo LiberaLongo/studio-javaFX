ea482fca60260f5bd5b5857c408ee719215ed364
liberalongo@gmail.com <liberalongo@gmail.com>
2018-07-25 12:09:06 +0200

    'sorgente predefinito'

--- JavafxSample.java
+++ JavafxSample.java
@@ -8,18 +8,18 @@ public class JavafxSample extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		//creating a Group object
-		Group group = new Group();
+		Group root = new Group();
 
 		//Creating a Scene by passing the group object, height and width
-		Scene scene = new Scene(group ,600, 300);
+		Scene scene = new Scene(root ,600, 300);
 
 		//setting color to the scene
 		scene.setFill(Color.BROWN);
 
-		//Setting the title to Stage.
-		primaryStage.setTitle("Sample Application");
+		//Setting the title to stage.
+		primaryStage.setTitle("Simple Application");
 
-		//Adding the scene to Stage
+		//Adding the scene to stage
 		primaryStage.setScene(scene);
 
 		//Displaying the contents of the stage
